fd3c0f0b7f275d5bcf4363e49e330ff819477513
Isa Mary <isa.kusuki@gmail.com>
2022-11-05 15:55:00 -0300

    Criação dos métodos update, delete, parse, consultById, parseList, list, listUserByTipo

--- src/main/java/br/com/gs/dao/UsuarioDAO.java
+++ src/main/java/br/com/gs/dao/UsuarioDAO.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import br.com.gs.exceptions.IdNotFoundException;
 import br.com.gs.to.EnderecoTO;
 import br.com.gs.to.PlanoTO;
 import br.com.gs.to.UsuarioTO;
@@ -43,15 +46,113 @@ public class UsuarioDAO {
 
 	}
 
-	public void update(UsuarioTO usuario) {
+	public void update(UsuarioTO usuario) throws SQLException, IdNotFoundException {
+		PreparedStatement stmt = conexao.prepareStatement(
+				"update t_charchan_usuario set nm_usuario = ?, ds_sobrenome = ?, nr_cpf = ?, nr_cnh = ?, "
+				+ "dt_nascimento = to_date(?,'dd/mm/yyyy'), nr_telefone = ?, ds_email = ?, ds_senha = ?, "
+				+ "ds_tipo = ? where id_usuario = ?");
 		
+		stmt.setString(1, usuario.getNome());
+		stmt.setString(2, usuario.getSobrenome());
+		stmt.setString(3, usuario.getCpf());
+		stmt.setString(4, usuario.getCnh());
+		stmt.setString(5, usuario.getDataNascimento());
+		stmt.setString(6, usuario.getTelefone());
+		stmt.setString(7, usuario.getEmail());
+		stmt.setString(8, usuario.getSenha());
+		stmt.setString(9, usuario.getTipo());
+		
+		int qtd = stmt.executeUpdate();
+		
+		if(qtd == 0)
+			throw new IdNotFoundException("Usuario não encontrado para atualização");
 	}
 	
-	public void delete(int id) {
+	public void delete(int id) throws SQLException, IdNotFoundException {
+		PreparedStatement stmt = conexao.prepareStatement(
+				"delete from t_charchan where id_usuario = ?");
 		
+		stmt.setInt(1, id);
+		int qtd = stmt.executeUpdate();
+		
+		if (qtd == 0)
+			throw new IdNotFoundException("Id não encontrado para remoção");
 	}
 	
-	public void consult() {
+	private UsuarioTO parse(ResultSet resultSet) throws SQLException {
+		int id = resultSet.getInt("id_usuario");
+		String nome = resultSet.getString("nm_nome");
+		String sobrenome = resultSet.getString("ds_sobrenome");
+		String cpf = resultSet.getString("nr_cpf");
+		String cnh = resultSet.getString("nr_cnh");
+		String dtNascimento = resultSet.getString("dt_nascimento");
+		String telefone = resultSet.getString("nr_telefone");
+		String email = resultSet.getString("ds_email");
+		String senha = resultSet.getString("ds_senha");
+		String tipo = resultSet.getString("ds_tipo");
 		
+		UsuarioTO usuario = new UsuarioTO(id, nome, sobrenome, cpf, cnh, dtNascimento, telefone, email, senha, tipo);
+		return usuario;
+	}
+	
+	/**
+	 * Pesquisa um usuário pela PK
+	 * @param id
+	 * @return
+	 * @throws SQLException
+	 * @throws IdNotFoundException
+	 */
+	public UsuarioTO consultById(int id) throws SQLException, IdNotFoundException {
+		PreparedStatement stmt = conexao.prepareStatement(
+				"select * from t_charchan_usuario where id_usuario = ?");
+		
+		stmt.setInt(1, id);
+		
+		ResultSet result = stmt.executeQuery();
+		
+		if (!result.next()) {
+			throw new IdNotFoundException("Usuário não encontrado");
+		}
+		
+		return parse(result);
+	}
+	
+	private List<UsuarioTO> parseList(ResultSet resultSet) throws SQLException{
+		List<UsuarioTO> lista = new ArrayList<UsuarioTO>();
+		
+		while (resultSet.next()) {
+			UsuarioTO usuario = parse(resultSet);
+			lista.add(usuario);
+		}
+		return lista;
+	}
+	
+	/**
+	 * Retorna todos os usuários cadastrados
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<UsuarioTO> list() throws SQLException{
+		PreparedStatement stmt = conexao.prepareStatement("select * from t_charchan_usuario");
+		
+		ResultSet result = stmt.executeQuery();
+		
+		return parseList(result);
+	}
+	
+	/**
+	 * Retorna todos os usuários por ds_tipo
+	 * @param tipo
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<UsuarioTO> listUserByTipo(String tipo) throws SQLException {
+		PreparedStatement stmt = conexao.prepareStatement("select * from t_charchan_usuario where ds_tipo = ?");
+		
+		stmt.setString(1, tipo);
+		
+		ResultSet result = stmt.executeQuery();
+		
+		return parseList(result);
 	}
 }
